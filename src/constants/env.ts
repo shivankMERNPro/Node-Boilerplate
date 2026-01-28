1dc5769b0815365ef4f74d2dc1162dd1474552bf
Shivank Singh <shivank.singh.developer@gmail.com>
2026-01-28 12:31:59 +0530

    Bug fixes

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const env = {
   nodeEnv: process.env.NODE_ENV || '',
-  port: Number(process.env.PORT) || 5000,
+  port: Number(process.env.PORT) || 8080,
   mongoUri: process.env.MONGO_URI as string,
   mongoDbName: process.env.MONGO_DB_NAME as string,
   mongoMaxPoolSize: Number(process.env.MONGO_MAX_POOL_SIZE) || 0,
